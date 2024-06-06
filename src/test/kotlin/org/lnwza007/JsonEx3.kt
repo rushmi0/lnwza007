963b7062db8aa632581a0a37032f4dded19d8413
milko <gcalku@gmail.com>
2024-06-06 23:36:36 +0700

    .

--- src/test/kotlin/org/lnwza007/JsonEx3.kt
+++ src/test/kotlin/org/lnwza007/JsonEx3.kt
@@ -1,51 +1,50 @@
 package org.lnwza007
 
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.*
+
+@Serializable
+sealed class Command
+
+@Serializable
+data class Event(val event: String, val data: JsonObject) : Command()
+
+@Serializable
+data class Request(val subscriptionId: String, val filtersX: List<JsonObject>) : Command()
+
+@Serializable
+data class Close(val subscriptionId: String) : Command()
 
 fun main() {
+
     val inputStrings = listOf(
         """["EVENT", {"event": "some_event", "data": {"key": "value"}}]""",
-        """["REQ", "subscription_id_123", {"filter1": "value1"}, {"filter2": "value2"}]""",
+        """["REQ", "subscription_id_123", [{"filter1": "value1"}, {"filter2": "value2"}]]""",
         """["CLOSE", "subscription_id_123"]"""
     )
 
-    for (inputString in inputStrings) {
-        val inputList = Json.decodeFromString<List<Any>>(inputString)
-        val command = inputList[0] as String
-
-        when (command) {
+    inputStrings.forEach { inputString ->
+        val jsonElement = Json.parseToJsonElement(inputString)
+        val command = when (val type = jsonElement.jsonArray[0].jsonPrimitive.content) {
             "EVENT" -> {
-                val eventJson = inputList[1] as JsonObject
-                handleEvent(eventJson)
+                val eventJson = jsonElement.jsonArray[1].jsonObject
+                Json.decodeFromJsonElement<Event>(eventJson)
             }
             "REQ" -> {
-                val subscriptionId = inputList[1] as String
-                val filters = inputList.subList(2, inputList.size).map { it as JsonObject }
-                handleRequest(subscriptionId, filters)
+                val subscriptionId = jsonElement.jsonArray[1].jsonPrimitive.content
+                val filtersJson = jsonElement.jsonArray[2].jsonArray.map { it.jsonObject }
+                Json.decodeFromJsonElement<Request>(Json.encodeToJsonElement(Request(subscriptionId, filtersJson)))
             }
             "CLOSE" -> {
-                val subscriptionId = inputList[1] as String
-                handleClose(subscriptionId)
+                val subscriptionId = jsonElement.jsonArray[1].jsonPrimitive.content
+                Close(subscriptionId)
             }
-            else -> {
-                println("Unknown command: $command")
-            }
+            else -> throw IllegalArgumentException("Unknown command type: $type")
+        }
+        when (command) {
+            is Event -> println("Handling event: $command")
+            is Request ->  println("Handling request for subscription ID: ${command.subscriptionId} with filters: ${command.filtersX}")
+            is Close -> println("Handling close request for subscription ID: ${command.subscriptionId}")
         }
     }
-}
-
-fun handleEvent(eventJson: JsonObject) {
-    // จัดการกับเหตุการณ์ที่ได้รับ
-    println("Handling event: $eventJson")
-}
-
-fun handleRequest(subscriptionId: String, filters: List<JsonObject>) {
-    // ดำเนินการตามคำขอและสร้างการสมัครสมาชิกใหม่
-    println("Handling request for subscription ID: $subscriptionId with filters: $filters")
-}
-
-fun handleClose(subscriptionId: String) {
-    // ยกเลิกการสมัครสมาชิก
-    println("Handling close request for subscription ID: $subscriptionId")
 }
